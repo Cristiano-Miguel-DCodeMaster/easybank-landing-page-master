36920537b6cac2e0df2eb9bcbc77e316945316b4
Cristiano-Miguel-DCodeMaster <cristianomiguel128@gmail.com>
2024-06-26 22:39:42 +0100

    I added some basic interaction and some scroll reveal animation

--- js/index.js
+++ js/index.js
@@ -22,3 +22,16 @@ setTimeout(() => {
     loader.style.display = "none";
 hider.style.display = "none";
 }, 7000);
+let sections = document.querySelectorAll('section')
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;    
+        let offset = sec.offsetTop - 150;    
+        let height = sec.offsetHeight;
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('me');
+        } else {
+            sec.classList.remove('me');
+        }
+    });
+}
